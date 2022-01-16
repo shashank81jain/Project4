9404971c19b117c5040df3dc460a5976d689cfd9
EC2 Default User <ec2-user@ip-172-31-43-14.ec2.internal>
2022-01-16 23:51:19 +0000

    Adding Kubernetes Files

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=shashank81jain/project4
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run project4k --image=$dockerpath --port=80 --labels app=project4k
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward pods/project4k 8080:80
